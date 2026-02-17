70d89d1b2b8ad7f48b8c6576af2b6e5d4cc311d6
rakhattttt <r_zhenigsbek@kbtu.kz>
2026-02-17 18:25:06 +0500

    Delete task1 directory

--- task1/inputproperties.ts
+++
@@ -1,9 +0,0 @@
-import {Component, input} from '@angular/core';
-
-@Component({
-  selector: 'app-user',
-  template: `<p>The user's occupation is {{occupation()}}</p><app-user occupation="Angular Developer"></app-user>`,
-})
-export class User {
-    occupation = input<string>();
-}
